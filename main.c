1867e6702774f1eb2e5d334da86707157e90d259
BooMario <gabriel.de.ac@hotmail.com>
2016-08-26 16:15:34 -0300

    Alterações em geral

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <pthread.h>
 
 //Funções relacionadas ao GPIO
 #include "lp.h"
@@ -31,23 +32,60 @@
 #define led_col3 14
 #define led_col4 15
 
+//Define até quantos elementos de aúdio de uma coluna podem ser colocados na fila
+#define list_size 10
+
+
+//Funções do programa
 void init(),desliga();
+//Funções que iniciam em uma nova thread:
+void * ler_bot(void * threadid), *update_leds(void * threadid);
 
 bool mat_bot[4][4],mat_led[4][4];
+int aux_bot;
 int i,j;
+
+//Fila circular, uma para cada coluna em reprodução
+Fila playlist[4];
+
+//Variáveis de thread!
+//Thread 1: ler botões
+//Thread 2: atualizar LED's
+pthread_t thread;
+pthread_t thread2;
+
+int rc;
+int rc2;
+
+long t;
+long t2;
+//Fim das variáveis de thread
+
 
 int main (){
 	init();
 	while(1){
-		//lógica aqui, tira esse break depois
-		break;
+		aux_bot=0;
+		for (i=0;i<4;i++){
+			for (j=0;j<4;j++){
+				if (mat_bot[i][j]==true){
+					mat_led[i][j] = true;
+					aux_bot++;
+					push(aux_bot, &playlist[i]);
+				}
+			}
+		}
+		
 	}
-	printf("Hello World!\n");
 	desliga();
+	return 0;
 }
 
-
 void init(){
+	for (i=0;i<4;i++){
+		iniciar(list_size ,&playlist[i]);
+	}
+	
 	//zerar matrizes de booleanos
 	for (i=0;i<4;i++){
 		for (j=0;j<4;j++){
@@ -58,13 +96,13 @@ void init(){
 	
 	//botões
 	GPIOExport(bot_lin1);
-	GPIODirection(bot_lin1, INPUT);
+	GPIODirection(bot_lin1, OUTPUT);
 	GPIOExport(bot_lin2);
-	GPIODirection(bot_lin2, INPUT);
+	GPIODirection(bot_lin2, OUTPUT);
 	GPIOExport(bot_lin3);
-	GPIODirection(bot_lin3, INPUT);
+	GPIODirection(bot_lin3, OUTPUT);
 	GPIOExport(bot_lin4);
-	GPIODirection(bot_lin4, INPUT);
+	GPIODirection(bot_lin4, OUTPUT);
 	
 	GPIOExport(bot_col1);
 	GPIODirection(bot_col1, INPUT);
@@ -93,6 +131,18 @@ void init(){
 	GPIODirection(led_col3, OUTPUT);
 	GPIOExport(led_col4);
 	GPIODirection(led_col4, OUTPUT);
+	
+	rc = pthread_create(&thread,NULL,ler_bot,(void*)t);
+	if (rc){
+		printf("Init. Failed! Cannot start thread ler_bot.\n");
+		exit(-1);
+	}
+	
+	rc2 = pthread_create(&thread2,NULL,update_leds,(void*)t2);
+	if (rc2){
+		printf("Init. Failed! Cannot start thread update_leds\n");
+		exit(-1);
+	}
 }
 
 void desliga(){
@@ -117,4 +167,17 @@ void desliga(){
 	GPIOUnexport(led_col2);
 	GPIOUnexport(led_col3);
 	GPIOUnexport(led_col4);
+	
+	
+	pthread_exit(NULL);
+}
+
+void * ler_bot(void * threadid)
+{
+
+}
+
+void * update_leds(void * threadid)
+{
+
 }
